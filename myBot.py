f118bd62ca1af498b0e5d0ecd1bd2479a615a038
ZePandaSan <zepandasan@gmail.com>
2025-09-27 14:33:20 +0200

    Adding few command

--- myBot.py
+++ myBot.py
@@ -1,38 +1,68 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv  # Pour charger les variables d'environnement
-import os                      # Pour accéder aux variables d'environnement
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+from pathlib import Path  # Pour gérer les chemins de fichiers
 
-# Charger les variables depuis le fichier .env
+# Chargement des variables d'environnement
 load_dotenv()
-
-# Récupérer le token
 TOKEN = os.getenv('TOKEN')
 
-# intents
+# Configuration des intents
 intents = discord.Intents.default()
-intents.message_content = True  
-intents.guilds = True 
+intents.message_content = True
+intents.guilds = True
 
-# bot creation 
+# Initialisation du bot
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Chemin vers le dossier des médias
+MEDIA_DIR = Path("media")
 
 @bot.event
 async def on_ready():
-    print(f"connected as {bot.user}")
+    """Log entry"""
+    print(f"Connecté en tant que {bot.user}")
 
 @bot.event
 async def on_message(message):
-    # avoid loop
+    # Ignore loop
     if message.author == bot.user:
         return
 
-    # Check if mentionned
+    # Répondre aux mentions
     if bot.user in message.mentions:
-        await message.channel.send("Réel")
+        gif_path = MEDIA_DIR / "mention.gif"
+        if gif_path.exists():  # Vérifie que le fichier existe
+            with open(gif_path, 'rb') as gif_file:
+                await message.channel.send(file=discord.File(gif_file, "mention.gif"))
+        else:
+            print(f"Fichier introuvable : {gif_path}")
 
-    
     await bot.process_commands(message)
 
+@bot.command(name="ohayo")
+async def ohayo(ctx):
+    """Ohayo username"""
+    await ctx.send(f"Ohayo {ctx.author.mention} !")
 
-bot.run(TOKEN)
+
+@bot.command(name="command")
+async def help_command(ctx):
+    help_message = (
+        "**Commandes disponibles :**\n"
+        "`!ohayo` : Ohayo username.\n"
+    )
+    await ctx.send(help_message)
+
+@bot.command(name="time")
+async def getTime(ctx):
+    time = datetime.now()
+    stringTime=time.strftime("%H:%M:%S")
+    await ctx.send(stringTime)
+
+
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
